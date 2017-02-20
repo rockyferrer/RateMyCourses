095aa8bc2b4adaaae2c94a3bd1e6cae42d74c0a6
ansermino <davidansermino@gmail.com>
2017-02-20 17:35:40 -0500

    Testing header import

--- js/login.js
+++ js/login.js
@@ -1,6 +1,11 @@
-$(function(){
+$(function() {
+    // Load header html into header element
+    $("#header").load("header.html");
+
     //toggles menu visibility
-    $('#register-btn').click(function() {
-        $('.register-file').css("display", "none");
+    $('#register-btn').click(function(e) {
+        e.preventDefault();
+        $(".register-field").toggle();
+        $("#login-btn").set
     });
 });
