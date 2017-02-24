311d39bb852241345e3c9d923ca8159890bf9a88
ansermin <david.ansermino@mail.utoronto.ca>
2017-02-23 20:02:25 -0500

    Added departments, faculty, form validation

--- js/login.js
+++ js/login.js
@@ -1,11 +1,43 @@
-$(function() {
-    // Load header html into header element
-    $("#header").load("header.html");
+/** jQuery **/
 
-    //toggles menu visibility
-    $('#register-btn').click(function(e) {
-        e.preventDefault();
-        $(".register-field").toggle();
-        $("#login-btn").set
-    });
+$(document).ready(function() {
+
+	//Toggles registration fields
+	$('#register-btn').click(function(e) {
+	    e.preventDefault();
+	    $(".register-field").toggle();
+	    $("#login-btn").toggle();
+		$("#create-account").toggle();
+		if($(".required-field").css("required")){
+			$(".required-field").css("required", "false");
+		}else{
+			$(".required-field").css("required", "true");
+		}
+		
+	});
+
+});
+
+/** Angular **/
+
+var app = angular.module("login", []);
+
+app.controller("departments", function($scope) {
+	$scope.department_list = [
+		"Computer Science",
+		"Mathematics",
+		"Business",
+		"Statistics",
+		"Life Sciences"
+	]
+});
+
+app.controller("faculties", function($scope) {
+	$scope.faculty_list = [
+		"Arts and Sciences",
+		"Applied Sciences and Engineering",
+		"Education",
+		"Law",
+		"Dentistry"
+	]
 });
