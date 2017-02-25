4ed13438043808252b35e13022658862f243ad2b
David Ansermino <davidansermino@gmail.com>
2017-02-24 21:34:56 -0500

    angular modules

---
+++ js/course.js
@@ -0,0 +1,23 @@
+/** jQuery **/
+
+$(document).ready(function() {
+
+
+
+});
+
+/** Angular **/
+
+var app = angular.module("course", []);
+
+app.controller("ratings", function($scope) {
+    $scope.rating = [{
+        "diffulty": 5,
+        "workload": 5,
+        "experience:": 5,
+        "average": 5,
+        "comment": "hsjaksdfhkjasdhfk"
+    }, {
+
+    }]
+});
