79d82bd4b20cfe3a7c5644cc666d5c3925117c9a
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-02-24 21:41:45 -0500

    added more user ratings

--- js/course.js
+++ js/course.js
@@ -12,12 +12,22 @@ var app = angular.module("course", []);
 
 app.controller("ratings", function($scope) {
     $scope.rating = [{
-        "diffulty": 5,
-        "workload": 5,
+        "difficulty": 2,
+        "workload": 3,
         "experience:": 5,
         "average": 5,
-        "comment": "hsjaksdfhkjasdhfk"
+        "comment": "Great course!"
     }, {
-
+        "difficulty": 2,
+        "workload": 5,
+        "experience:": 5,
+        "average": 4,
+        "comment": "Very interesting course"
+    }, {
+        "difficulty": 2.5,
+        "workload": 3,
+        "experience:": 5,
+        "average": 4.5,
+        "comment": "Learned alot from this class."
     }]
 });
