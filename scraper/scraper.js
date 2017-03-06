26245a97c97e355755e16b7cfd2cd0ada0bad57b
ansermino <davidansermino@gmail.com>
2017-03-06 00:25:38 -0500

    Changed mongoose save function

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -10,16 +10,20 @@ var url = 'https://cobalt.qas.im/api/1.0/courses/filter' + '?key=' + key + "&q=d
 
 var db = mongoose.connect(MONGODB);
 request(url, function(error, reponse, body){
+	parseJSON(body);
+});
+
+var data = [];
+
+function parseJSON(body){
 	var result = JSON.parse(body);
 	console.log(result[0]);
 	result.forEach(function(e){
 		pullData(e);
-		if(result.indexOf(e) == result.length - 1){
-			mongoose.connection.close();
-		}
 	});
-});
+	saveToDB(data);
 
+};
 
 function pullData(result){
 	console.log(result);
@@ -36,10 +40,69 @@ function pullData(result){
 	console.log("*********************************************");
 	console.log(c);
 	console.log("=============================================");
-	c.save(function(err){console.log(err)});
-
+	data.push(c);
 };
 
-function addToDB(course){
-	course.save(function(err){console.log(err)});
+function saveToDB(courses){
+	/*course.create(courses, function (err) {
+		console.log(err);	
+	});*/
+	courses.forEach(function(e) {
+		e.save(function(err){
+		    if(err){
+		        console.log(err);
+		    }
+		    else{
+		        console.log('Sucess!');
+		    }
+		});	
+	});
+	mongoose.connection.close();
+	//course.save(function(err){console.log(err)});
 };
+
+// Logging methods
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + MONGODB);
+});
+
+mongoose.connection.on('error', function(error){
+    console.log('Mongoose connection error: ' + error);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected.');
+});
+
+// Clean shutdown methods
+gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+    })
+};
+
+function save(dataObj){
+    try{
+        var newCourse = new course({
+		courseCode: 'CSCXXX',
+		title: result.name,
+		department: 'Computer Science',
+		description: result.description,
+		popularTags: [],
+		classSize: 9999, //TODO: Fix this
+		ratings: []
+	});
+      
+        newBuilding.save(function(err){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log('Sucess!');
+            }
+        });
+    }
+    catch (err){
+        console.log("Undefined behaviour: " + err);
+    }
+}
