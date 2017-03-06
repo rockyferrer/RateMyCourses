cb78da2e4b7fd0c21892f1b60b8deba00052368e
ansermino <davidansermino@gmail.com>
2017-03-05 22:53:33 -0500

    Scraper calls working, issues with mongo save

--- model.js
+++ model.js
@@ -1,16 +1,33 @@
 var mongoose = require('mongoose');
 
+// TODO: Change types to schema
+
 var courseSchema = new mongoose.Schema({
 	courseCode: {type: String, required: true},
 	title: {type: String, required: true},
-	department: {type: departmentSchema, require: true},
+	department: {type: String, require: true},
 	description: {type:String, require: true},
-	popularTags: {type: [{type: mongoose.Schema.type.ObjectId, ref: 'Tag'}]},
+	popularTags: [String],
 	classSize: {type: Number},
-	ratings: {[{type: mongoose.Schema.type.ObjectId, ref: 'Rating']}
+	ratings: [String]
+});
+
+var tagSchema = new mongoose.Schema({
+	name: {type: String, required: true}
+});
+
+var ratingSchema = new mongoose.Schema({
+	comment: {type: String, required: true}
+});
+
+var departmentSchema = new mongoose.Schema({
+	name: {type: String, required: true}
 });
 
 var course = mongoose.model('Course', courseSchema);
+var tag = mongoose.model('Tag', tagSchema);
+var rating = mongoose.model('Rating', ratingSchema);
+var department = mongoose.model('Department', departmentSchema);
 
 
-module.exports = {'Course': Course};
+module.exports = {Course: course, Tag: tag, Rating: rating, Department: department};
--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,36 +1,45 @@
 var request = require('request');
 var mongoose = require('mongoose');
+var models = require('../model.js');
 
-var MONGODB = 'mongo://localhost/Team23-RateMyCourses';
+var MONGODB = 'mongodb://localhost/Team23-RateMyCourses';
 var key = '9iDLLnSQRMwapjbMs5Yb7tnwyqAxK5ud';
+var course = models.Course;
 
-var url = 'https://cobalt.qas.im/api/1.0/courses/filter' + '?key=' + key + "&q=department:\"Computer Science&limit=100";
+var url = 'https://cobalt.qas.im/api/1.0/courses/filter' + '?key=' + key + "&q=department:\"Computer Science&limit=10";
 
-var db = mongoose.connect('MONGODB');
-
+var db = mongoose.connect(MONGODB);
 request(url, function(error, reponse, body){
 	var result = JSON.parse(body);
-	console.log(Object.keys(result).length);
-	result.forEach(console.log(this.code));
-	pullData(result);
+	console.log(result[0]);
+	result.forEach(function(e){
+		pullData(e);
+		if(result.indexOf(e) == result.length - 1){
+			mongoose.connection.close();
+		}
+	});
 });
 
 
 function pullData(result){
-	var course ={
-		courseCode: result.code,
+	console.log(result);
+
+	var c = new course({
+		courseCode: 'CSCXXX',
 		title: result.name,
-		department: result.department,
+		department: 'Computer Science',
 		description: result.description,
 		popularTags: [],
-		classSize: result.meetingSections[0].size,
+		classSize: 9999, //TODO: Fix this
 		ratings: []
-	};
-
-	addToDB(course);
+	});
+	console.log("*********************************************");
+	console.log(c);
+	console.log("=============================================");
+	c.save(function(err){console.log(err)});
 
 };
 
 function addToDB(course){
-	db.Courses.insert(course);
+	course.save(function(err){console.log(err)});
 };
