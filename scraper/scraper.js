eafd4f0d3c8a953110a6b57b5d43b9025e98825d
ansermino <davidansermino@gmail.com>
2017-03-12 16:37:11 -0400

    Setup courses route and basic config

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -16,55 +16,6 @@ var CourseScraper = require('./CourseScraper');
 
 var cs = new CourseScraper(db, 'Computer Science');
 
-
-
-
-/*function parseJSON(body){
-	var result = JSON.parse(body);
-	console.log(result[0]);
-	result.forEach(function(e){
-		pullData(e);
-	});
-	saveToDB(data);
-
-};
-
-function pullData(result){
-	console.log(result);
-
-	var c = new course({
-		courseCode: 'CSCXXX',
-		title: result.name,
-		department: 'Computer Science',
-		description: result.description,
-		popularTags: [],
-		classSize: 9999, //TODO: Fix this
-		ratings: []
-	});
-	console.log("*********************************************");
-	console.log(c);
-	console.log("=============================================");
-	data.push(c);
-};
-
-function saveToDB(courses){
-	/*course.create(courses, function (err) {
-		console.log(err);	
-	});*/
-/*courses.forEach(function(e) {
-		e.save(function(err){
-		    if(err){
-		        console.log(err);
-		    }
-		    else{
-		        console.log('Sucess!');
-		    }
-		});	
-	});
-	mongoose.connection.close();
-	//course.save(function(err){console.log(err)});
-};*/
-
 // Logging methods
 mongoose.connection.on('connected', function() {
     console.log('Mongoose connected to ' + MONGODB);
--- server.js
+++ server.js
@@ -1,19 +1,78 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var models = require('./model.js');
 
 var app = express();
 
-var MONGODB = 'NOT SET'
+var MONGODB = 'mongodb://localhost/Team23-RateMyCourses';
 
+// Create mongo connection and setup event handlers
 mongoose.connect(MONGODB);
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + MONGODB);
+});
+mongoose.connection.on('error', function(error) {
+    console.log('Mongoose connection error: ' + error);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected.');
+});
 
+// Create mongoose schemas
+// TODO: Add the other schemas once defined
+var Course = mongoose.connection.model('Course', models.courseSchema);
 
-function getCourses(req, res){
-	var courses = db.getCourses();
-	res.send(JSON.stringify(courses));
-}
+//Set up static path so /img will be treated as assets/img
+app.use(express.static(__dirname + '/assets'));
+// Use HTTP port
+app.listen(8080, function() {
+    console.log("Listening on port 8080.")
+});
+
+/**
+ * Returns matching courses. 
+ * Optional course code and department parameters.
+ */
+function getCourses(req, res) {
+    console.log(req.query);
+    var code = req.query.courseCode;
+    var dept = req.query.department;
+    if (code && dept) {
+        Course.find({ courseCode: req.query.courseCode, department: req.query.department }, function(err, courses) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(courses);
+        });
+    } else if (code) {
+        Course.find({ courseCode: code }, function(err, courses) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(courses);
+        });
+    } else if (dept) {
+        Course.find({ department: dept }, function(err, courses) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(courses);
+        });
+    } else {
+        Course.find(function(err, courses) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(courses);
+        });
+    }
+
+};
 
 app.get('/courses', getCourses);
+// Angular default request
+app.get('*', function(req, res) {
+    res.sendFile(__dirname + '/assets/home.html');
+});
