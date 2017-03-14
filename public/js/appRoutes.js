da010537040b63fe44bbe3853a9570a1d8971129
David Ansermino <davidansermino@gmail.com>
2017-03-14 17:22:02 -0400

    Added course

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -4,24 +4,28 @@ angular.module('appRoutes', ['LoginCtrl', 'DepartmentCtrl', 'CourseCtrl']).confi
 
     // home page
         .when('/', {
+        title: 'RateMyCourses - Knowledge Is Power',
         templateUrl: 'views/home.html',
         controller: 'MainController'
     })
 
     // Login page
     .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginController'
-    })
-
-    // Department Page
-    .when('/dept/:department', {
-        templateUrl: 'views/department.html',
-        controller: 'DepartmentController'
-    })
+            title: 'RateMyCourses - Login',
+            templateUrl: 'views/login.html',
+            controller: 'LoginController'
+        })
+        //TODO: ADD CORRECT TITLE
+        // Department Page
+        .when('/dept/:department', {
+            title: 'RateMyCourses - ',
+            templateUrl: 'views/department.html',
+            controller: 'DepartmentController'
+        })
 
     // Course Page
     .when('/course/:courseCode', {
+        title: 'RateMyCourses',
         templateUrl: 'views/course.html',
         controller: 'CourseController'
     });
