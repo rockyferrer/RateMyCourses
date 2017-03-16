5c7182658c52b06b26c15c2ca5f1679b30f79be0
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-03-16 00:49:27 -0400

    added tag selection

--- public/js/controllers/CourseCtrl.js
+++ public/js/controllers/CourseCtrl.js
@@ -4,4 +4,69 @@ angular.module('CourseCtrl', []).controller('CourseController', function($scope,
         $scope.course = data.data;
     });
 
+    $scope.tags = [
+        'Cool',
+        'Fun',
+        'Great TAs',
+        'Exclusive Tag For Best Course Ever',
+        'Fair',
+        'Great Professor',
+        'Lots of Smiles',
+        'Spicy',
+        'Please Give Me a Good Mark',
+        'Would Take Again'
+    ]
+
+    $scope.currentTags = [];
+
+    $scope.containsNulls = function() {
+        for (var i = 0; i < $scope.currentTags.length; i++) {
+            if ($scope.currentTags[i] == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //TODO: there is a bug when you select 3 tags, then deselect one and then
+    // try to reselect it again. Otherwise, it works perfectly if you stick with
+    // the first 3 tags u choose
+    $scope.chooseTag = function($event, $index) {
+        var chosenEl = angular.element($event.target);
+        var chosenTag = $scope.tags[$index];
+        console.log($scope.currentTags.indexOf(chosenTag));
+        //if the tag has not been chosen and less than three overall have been chosen
+        if ($scope.currentTags.indexOf(chosenTag) == -1 && $scope.currentTags.length < 3) {
+            chosenEl.css('background-color', '#337ab7');
+            console.log('chose: ' + chosenTag);
+            if ($scope.containsNulls()) {
+                for (var i = 0; i < $scope.currentTags.length; i++) {
+                    if ($scope.currentTags[i] == null) {
+                        $scope.currentTags[i] = chosenTag;
+                        break;
+                    }
+                }
+            } else {
+                $scope.currentTags.push(chosenTag);
+            }
+            console.log('stored: ' + chosenTag);
+        } else if ($scope.currentTags.indexOf(chosenTag) != -1) {
+            console.log("deselecting");
+            $scope.currentTags[$scope.currentTags.indexOf(chosenTag)] = null;
+            chosenEl.css('background-color', 'rgba(55, 64, 70, 0.4)');
+        }
+    };
+
+    $scope.postRating = function() {
+        if ($scope.currentTags.length > 0) {
+            for (var i = 0; i < $scope.currentTags.length; i++) {
+                console.log('the ' + i + 'th element is ' + $scope.currentTags[i]);
+            }
+        } else {
+            console.log('no tags chosen');
+        }
+
+        // return $scope.currentTags;
+    };
+
 });
