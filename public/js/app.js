7414c0d8f5b8c3e6af05910caf3306a6ecfd451a
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-03-16 01:03:33 -0400

    fixed conflict

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,6 @@ angular.module('rateMyCourses', [
     'MainCtrl',
     'LoginCtrl', 'LoginService',
     'DepartmentCtrl', 'DepartmentService',
-<<<<<<< HEAD
-    'CourseCtrl', 'CourseService'
-]);
-=======
     'CourseCtrl', 'CourseService',
     'LandingCtrl', 'LandingService'
 ]);
->>>>>>> 9414ac1993053cff7ab960435cfd1542608fa41d
