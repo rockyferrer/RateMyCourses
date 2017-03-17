3ce995ecf745b2bac061a0ec943768e292c5447e
felipesantoz <felipe.santoz2013@gmail.com>
2017-03-16 21:22:18 -0400

    small adjustment on search control

--- public/js/controllers/SearchCtrl.js
+++ public/js/controllers/SearchCtrl.js
@@ -1,6 +1,6 @@
 angular.module('SearchCtrl', []).controller('SearchController', function($scope, $routeParams, SearchService) {
 
-    $scope.courseResults = DepartmentService.searchResults($routeParams.query);
+    $scope.courseResults = SearchService.searchResults($routeParams.query);
     $scope.cssFilename = "searchresults";
 
 });
